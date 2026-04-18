de0c8e547ad1d028de60708b680c5a66ec1d915f
Sydney omori <sydneyomori@gmail.com>
2026-04-18 18:38:59 +0300

    feat: add getData function to fetch the data

--- wordly.js
+++ wordly.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/hello"
+
+async function getData(word){
+  const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+  try{
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)
+  }catch(error){
+    console.log(error)
+  }
+}
+
+getData("hello")
+
+
+
