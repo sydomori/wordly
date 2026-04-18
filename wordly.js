945b913e4b51b33d96a4a715c5da1c06da5b62dc
Sydney omori <sydneyomori@gmail.com>
2026-04-18 19:11:16 +0300

    feat: add functionality to getData to get a myWord object

--- wordly.js
+++ wordly.js
@@ -1,13 +1,33 @@
 const url = "https://api.dictionaryapi.dev/api/v2/entries/en/hello"
 
+const searchButton = document.querySelector(".search-btn");
+const searchInput = document.querySelector(".search-input");
+
+searchButton.addEventListener("click", () => {
+  const word = document.querySelector(".search-input").value;
+  getData(word);
+  searchInput.value = "";
+});
+
+
 async function getData(word){
   const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
   try{
     const response = await fetch(url)
     const data = await response.json()
     console.log(data)
+
+    let myWord = {
+      word: data[0].word,
+      partOfSpeech: data[0].meanings[0].partOfSpeech,
+      definition1: data[0].meanings[0].definitions[0].definition,
+      audio: data[0].phonetics[0].audio
+    }
+
+    console.log(myWord);
+
   }catch(error){
-    console.log(error)
+    console.log(error);
   }
 }
 
